b16215542d03b1256ceb57783a2e6ebabd597e01
Majid khan <khan.pk91@gmail.com>
2023-04-07 04:54:22 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  let currDate = new Date();       //Testing: year,month,date,hour1-24
+  currDate = currDate.getHours();
+  
+  let greeting = '';
+  const cssStyle = {};
+   
+  
+      if (currDate >= 1 && currDate <= 11){
+        greeting = "Good Morning"
+        cssStyle.color = "green"
+      }else if (currDate >= 12 && currDate <= 19 ){
+        greeting = "Good Afternoon" 
+        cssStyle.color = "orange"
+      } else {
+        cssStyle.color = "black";
+        greeting = "Good Night"
+      }
+
+    return (
+     <>
+      <div>
+      <h1>Hello, <span style={cssStyle}> {greeting} </span></h1>
+      </div>      
+     </>
+      );
+    };
+    
+      
 export default App;
